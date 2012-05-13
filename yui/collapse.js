52385c712dfcf88bef86e72fbd4ade897347431a
Kevin Isom <kevin.isom@gmail.com>
2012-05-13 20:40:11 +1200

    adding some scaffolding code

---
+++ yui/collapse.js
@@ -0,0 +1,60 @@
+YUI.add('collapse', function(Y){
+
+	var Collapse = function(config){
+		Collapse.superclass.constructor.apply(this, arguments);
+	}
+
+	Collase.name = 'collapse';
+
+	Collase.ATTRS = {
+
+	},
+
+	Y.extend(MyWidget, Y.Widget, {
+
+		initializer: function() {
+			
+		},
+
+		destructor : function() {
+
+		},
+
+		renderUI : function() {
+		
+		},
+
+		bindUI : function() {
+
+		},
+
+		syncUI : function() {
+
+		},
+
+		//Module methods
+		dimension : function(){
+
+		},
+
+		show : function(){
+
+		},
+
+		hide : function(){
+
+		},
+
+		reset : function(){
+
+		},
+
+		transition : function(){
+
+		},
+
+		toggle : function(){
+
+		}
+
+}, "2.0.3", {requires:['widget']}););
